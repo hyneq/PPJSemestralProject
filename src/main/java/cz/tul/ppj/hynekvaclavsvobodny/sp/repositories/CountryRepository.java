82a13e1fb75efa8a102f8406df35d16581b5f5b1
hyneq <hynek.vaclav.svobodny@tul.cz>
2025-05-11 00:49:19 +0200

    Updated CountryRepository so it includes findByName and findByCode methods

--- src/main/java/cz/tul/ppj/hynekvaclavsvobodny/sp/repositories/CountryRepository.java
+++ src/main/java/cz/tul/ppj/hynekvaclavsvobodny/sp/repositories/CountryRepository.java
@@ -3,11 +3,17 @@ package cz.tul.ppj.hynekvaclavsvobodny.sp.repositories;
 import cz.tul.ppj.hynekvaclavsvobodny.sp.data.Country;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CountryRepository extends NumberIdDataModelRepository<Country> {
 
     Country getByCode(String code);
 
+    Optional<Country> findByCode(String code);
+
     Country getByName(String name);
+
+    Optional<Country> findByName(String name);
 
 }
