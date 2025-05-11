514c3f5db00ea3a7a26afb2c46e893f0c945f316
hyneq <hynek.vaclav.svobodny@tul.cz>
2025-05-11 18:56:16 +0200

    Added WeatherController

---
+++ src/main/java/cz/tul/ppj/hynekvaclavsvobodny/sp/controllers/WeatherController.java
@@ -0,0 +1,25 @@
+package cz.tul.ppj.hynekvaclavsvobodny.sp.controllers;
+
+import cz.tul.ppj.hynekvaclavsvobodny.sp.data.Measurement;
+import cz.tul.ppj.hynekvaclavsvobodny.sp.services.WeatherService;
+import org.apache.coyote.Request;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/weather")
+public class WeatherController {
+
+    @Autowired
+    WeatherService service;
+
+    @PostMapping("/update/{city_id}")
+    public ResponseEntity<List<Measurement>> updateMeasurements(Integer cityId) {
+        return ResponseEntity.ofNullable(service.updateMeasurements(cityId));
+    }
+}
