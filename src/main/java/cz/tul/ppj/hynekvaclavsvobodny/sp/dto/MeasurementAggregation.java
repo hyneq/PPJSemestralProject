eab458c0d963caa78515a00e43724df40ed4c0f4
hyneq <hynek.vaclav.svobodny@tul.cz>
2025-05-15 19:24:27 +0200

    Converted MeasurementAggregation to a record

--- src/main/java/cz/tul/ppj/hynekvaclavsvobodny/sp/dto/MeasurementAggregation.java
+++ src/main/java/cz/tul/ppj/hynekvaclavsvobodny/sp/dto/MeasurementAggregation.java
@@ -2,67 +2,17 @@ package cz.tul.ppj.hynekvaclavsvobodny.sp.dto;
 
 import java.util.Objects;
 
-public class MeasurementAggregation {
-    private final double temp;
-    private final double tempFeelsLike;
-    private final double tempMin;
-    private final double tempMax;
-    private final int pressure;
-    private final int humidity;
-    private final double windSpeed;
-    private final double windGust;
-
-    public MeasurementAggregation(double temp, double tempFeelsLike, double tempMin, double tempMax, int pressure, int humidity, double windSpeed, double windGust) {
-        this.temp = temp;
-        this.tempFeelsLike = tempFeelsLike;
-        this.tempMin = tempMin;
-        this.tempMax = tempMax;
-        this.pressure = pressure;
-        this.humidity = humidity;
-        this.windSpeed = windSpeed;
-        this.windGust = windGust;
-    }
-
-    public double getTemp() {
-        return temp;
-    }
-
-    public double getTempFeelsLike() {
-        return tempFeelsLike;
-    }
-
-    public double getTempMin() {
-        return tempMin;
-    }
-
-    public double getTempMax() {
-        return tempMax;
-    }
-
-    public int getPressure() {
-        return pressure;
-    }
-
-    public int getHumidity() {
-        return humidity;
-    }
-
-    public double getWindSpeed() {
-        return windSpeed;
-    }
-
-    public double getWindGust() {
-        return windGust;
-    }
+public record MeasurementAggregation(double temp, double tempFeelsLike, double tempMin, double tempMax, int pressure,
+                                     int humidity, double windSpeed, double windGust) {
 
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof MeasurementAggregation that)) return false;
-        return Double.compare(getTemp(), that.getTemp()) == 0 && Double.compare(getTempFeelsLike(), that.getTempFeelsLike()) == 0 && Double.compare(getTempMin(), that.getTempMin()) == 0 && Double.compare(getTempMax(), that.getTempMax()) == 0 && getPressure() == that.getPressure() && getHumidity() == that.getHumidity() && Double.compare(getWindSpeed(), that.getWindSpeed()) == 0 && Double.compare(getWindGust(), that.getWindGust()) == 0;
+        return Double.compare(temp(), that.temp()) == 0 && Double.compare(tempFeelsLike(), that.tempFeelsLike()) == 0 && Double.compare(tempMin(), that.tempMin()) == 0 && Double.compare(tempMax(), that.tempMax()) == 0 && pressure() == that.pressure() && humidity() == that.humidity() && Double.compare(windSpeed(), that.windSpeed()) == 0 && Double.compare(windGust(), that.windGust()) == 0;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getTemp(), getTempFeelsLike(), getTempMin(), getTempMax(), getPressure(), getHumidity(), getWindSpeed(), getWindGust());
+        return Objects.hash(temp(), tempFeelsLike(), tempMin(), tempMax(), pressure(), humidity(), windSpeed(), windGust());
     }
 }
