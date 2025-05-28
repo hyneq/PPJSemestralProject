68f7306c78b98a03cc4ad5cea90a5535da006e77
Hynek V. Svobodny <hynekv.svobodny@gmail.com>
2025-05-29 00:27:52 +0200

    Revert "Made data.sql idempotent"

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
 
 -- Demo countries
-INSERT IGNORE INTO COUNTRY(code, name)
+INSERT INTO COUNTRY(code, name)
 VALUES ('CZ', 'Czech Republic'),
-       ('US', 'Un ited States'),
+       ('US', 'United States'),
        ('UK', 'United Kingdom'),
        ('DE', 'Germany');
 
 -- Demo cities
-INSERT IGNORE INTO CITY(id, name, country_code)
+INSERT INTO CITY(id, name, country_code)
 VALUES (3071961, 'Liberec', 'CZ'),
        (3067696, 'Praha', 'CZ'),
        (5128581, 'New York', 'US'),
