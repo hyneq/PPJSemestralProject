eaaf4bb81ed98bc62b35bbdd95b9ef245f99b884
hyneq <hynek.vaclav.svobodny@tul.cz>
2025-05-29 10:02:10 +0200

    Updated error handling in controllers

--- src/main/java/cz/tul/ppj/hynekvaclavsvobodny/sp/exceptions/ObjectAlreadyExistsException.java
+++ src/main/java/cz/tul/ppj/hynekvaclavsvobodny/sp/exceptions/ObjectAlreadyExistsException.java
@@ -1,7 +1,10 @@
 package cz.tul.ppj.hynekvaclavsvobodny.sp.exceptions;
 
 import cz.tul.ppj.hynekvaclavsvobodny.sp.data.IDataModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(code = HttpStatus.CONFLICT, reason = "The object already exists in the database")
 public class ObjectAlreadyExistsException extends DataModelException {
     public ObjectAlreadyExistsException(IDataModel<?> obj) {
         super(
--- src/main/java/cz/tul/ppj/hynekvaclavsvobodny/sp/exceptions/ObjectDoesNotExistException.java
+++ src/main/java/cz/tul/ppj/hynekvaclavsvobodny/sp/exceptions/ObjectDoesNotExistException.java
@@ -1,7 +1,10 @@
 package cz.tul.ppj.hynekvaclavsvobodny.sp.exceptions;
 
 import cz.tul.ppj.hynekvaclavsvobodny.sp.data.IDataModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "The object does not exist in the database")
 public class ObjectDoesNotExistException extends DataModelException {
     public ObjectDoesNotExistException(IDataModel<?> obj) {
         super(
